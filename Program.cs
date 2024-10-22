a5b0c22c3f275e6da905a2eeee2d0fe6bb35d896
arrrpii <117971249+arrrpii@users.noreply.github.com>
2024-10-22 15:55:50 +0400

    Add files via upload
    Palindrome, Merge Sort

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using ArpineAmiryan;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static bool Palindrome(string str)
+    {
+        for (int i = 0; i < str.Length / 2; i++)
+        {
+            if (str[i] != str[str.Length - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Type the string");
+        string input = Console.ReadLine().ToLower();
+        bool result = Palindrome(input);
+        Console.WriteLine(result);
+
+        int[] array = { 12, 6, 4, 52, 0, 8 };
+        foreach (var i in array)
+        {
+            Console.WriteLine(i + " ");
+        }
+        Console.WriteLine();
+
+
+        SortingAlgorithms.MergeSort(array, 0, array.Length - 1);
+        foreach (var i in array)
+        {
+            Console.WriteLine(i + " ");
+        }
+        Console.WriteLine();
+
+    }
+}
---
+++ SortingAlgorithms.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArpineAmiryan
+{
+    internal class SortingAlgorithms
+    {
+        public static void MergeSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+                MergeSort(arr, left, middle);
+                MergeSort(arr, middle + 1, right);
+                Mergearr(arr, left, middle, right);
+            }
+        }
+
+        private static void Mergearr(int[] arr, int left, int middle, int right)
+        {
+            int[] temp = new int[arr.Length];
+            for (int i = left; i <= right; i++)
+            {
+                temp[i] = arr[i];
+            }
+            int j = left;
+            int k = middle + 1;
+            int l = left;
+            while (j <= middle && k <= right)
+            {
+
+
+                if (temp[j] <= temp[k])
+                {
+                    arr[l] = temp[j];
+                    j++;
+                }
+                else
+                {
+                    arr[l] = temp[k];
+                    k++;
+                }
+                l++;
+            }
+            while (j <= middle)
+            {
+                arr[l] = temp[j];
+                l++;
+                j++;
+            }
+            while (k <= right)
+            {
+                arr[l] = temp[k];
+                l++;
+                k++;
+            }
+        }
+    }
+}
